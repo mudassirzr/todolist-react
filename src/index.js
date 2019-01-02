e67369757ac57724bdedb5ab6cfe623bade1b213
Mudasir Zahoor <mudassirzahoor14@gmail.com>
2019-01-02 23:26:59 +0530

    Sort functionality added

--- src/index.js
+++ src/index.js
@@ -20,13 +20,11 @@ TodoApp
 		- TodoListItem #N
 	- TodoForm
 */
-
-// var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self, call) {if (!self) {throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call && (typeof call === "object" || typeof call === "function") ? call : self;}function _inherits(subClass, superClass) {if (typeof superClass !== "function" && superClass !== null) {throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);}subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;}
 var todoItems = [];
-todoItems.push({index: 1, value: "learn react", done: false});
-todoItems.push({index: 2, value: "Go shopping", done: true});
-todoItems.push({index: 3, value: "buy flowers", done: true});
-
+todoItems.push({index: 1, value: "learn react", done: false, date: new Date()});
+todoItems.push({index: 2, value: "Go shopping", done: true, date: new Date()});
+todoItems.push({index: 3, value: "buy flowers", done: true, date: new Date()});
+// console.log(todoItems);
 class TodoList extends React.Component {
   render () {
     var items = this.props.items.map((item, index) => {
@@ -37,6 +35,15 @@ class TodoList extends React.Component {
     return (
     	<div className="todo-list">
     	<h2>Your ToDo List</h2>
+    	<div className="filter-block">
+    		<label>Filter By</label>
+    		<div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+			  <button type="button" className="btn btn-secondary" onClick={this.props.sortCreatedOld}>Created (Oldest)</button>
+			  <button type="button" className="btn btn-secondary" onClick={this.props.sortCreatedNew}>Created (Latest)</button>
+			  <button type="button" className="btn btn-secondary">Completed (Oldest)</button>
+			  <button type="button" className="btn btn-secondary">Completed (Latest)</button>
+			</div>
+    	</div>
       	<ul className="list-group"> {items} </ul>
       	</div>
     );
@@ -94,10 +101,10 @@ class TodoForm extends React.Component {
       <form ref="form" onSubmit={this.onSubmit}>
         <div className="input-group mb-3">
 			<input 
-			type="text"
-			ref="itemName"
-			className="form-control todo-input" 
-			placeholder="Enter your todo"
+				type="text"
+				ref="itemName"
+				className="form-control todo-input" 
+				placeholder="Enter your todo"
 			/>
 			<div className="input-group-append">
 				<button className="btn btn-success" type="submit">Add</button>
@@ -120,14 +127,18 @@ class TodoApp extends React.Component {
     this.addItem = this.addItem.bind(this);
     this.removeItem = this.removeItem.bind(this);
     this.markTodoDone = this.markTodoDone.bind(this);
+    this.sortCreatedOld = this.sortCreatedOld.bind(this);
+    this.sortCreatedNew = this.sortCreatedNew.bind(this);
     this.state = {todoItems: todoItems};
   }
   addItem(todoItem) {
     todoItems.unshift({
       index: todoItems.length+1, 
       value: todoItem.newItemValue, 
-      done: false
+      done: false,	
+      date: new Date()
     });
+    //console.log(todoItems);
     this.setState({todoItems: todoItems});
   }
   removeItem (itemIndex) {
@@ -139,14 +150,24 @@ class TodoApp extends React.Component {
     todoItems.splice(itemIndex, 1);
     todo.done = !todo.done;
     todo.done ? todoItems.push(todo) : todoItems.unshift(todo);
-    this.setState({todoItems: todoItems});  
+    this.setState({todoItems: todoItems});
+  }
+  sortCreatedOld () {
+  	this.state.todoItems.sort((a,b)=>a.date>b.date);
+  	// console.log(todoItems);
+  	this.setState({todoItems:todoItems});
+  }
+  sortCreatedNew () {
+  	this.state.todoItems.sort((a,b)=>a.date<b.date);
+  	// console.log(todoItems);
+  	this.setState({todoItems:todoItems});
   }
   render() {
     return (
       <div id="main">
         <TodoHeader />
         <TodoForm addItem={this.addItem} />
-      	<TodoList items={this.props.initItems} removeItem={this.removeItem} markTodoDone={this.markTodoDone}/>
+      	<TodoList items={this.props.initItems} removeItem={this.removeItem} markTodoDone={this.markTodoDone} sortCreatedOld={this.sortCreatedOld} sortCreatedNew={this.sortCreatedNew}/>
       </div>
     );
   }
